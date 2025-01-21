3f649ab3867a2b0e0f6194a22be569add5a81384
Antony Sajan <antonysajant@gmail.com>
2025-01-21 19:36:17 +0530

    Initial commit

---
+++ demo_app/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text("Hello my niggas"),
+          elevation: 0,
+        ),
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: EdgeInsets.all(25),
+            child: Icon(
+              Icons.access_alarm,
+              color: Colors.white,
+              size: 64,
+              )
+          ),
+        )
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
